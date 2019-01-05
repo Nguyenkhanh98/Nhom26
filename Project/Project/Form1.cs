5454a8d2781eef448a42029e12224e67a33a1dbf
Nguyen Huu Khanh <nhkhanh861998@gmail.com>
2019-01-05 15:12:17 +0700

    tạo tabcontrol

--- Project/Project/Form1.cs
+++ Project/Project/Form1.cs
@@ -16,5 +16,15 @@ namespace Project
         {
             InitializeComponent();
         }
+
+        private void tabPage5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
